06f1bde73cb5591211e9a4081de701f46a9a91cb
João Victor Olivo <jvso@yahoo.com>
2025-10-19 18:16:30 -0300

    functional register-login endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
 dotenv.config();
+
+import auth from 'router/auth';
+
 const app = express();
 
 app.use(express.json());
 
 // Rotas
+app.use('', auth);
 
 // Inicialização
 const PORT = process.env.PORT || 8800;
