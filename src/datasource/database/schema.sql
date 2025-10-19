99a1bfc0452c7f2deb04aa7ba8d06482e5a045a7
João Victor Olivo <jvso@yahoo.com>
2025-10-19 20:45:28 -0300

    schema.sql finished and updated

--- src/datasource/database/schema.sql
+++ src/datasource/database/schema.sql
@@ -1,8 +1,63 @@
 DROP TABLE IF EXISTS admin;
+DROP TABLE IF EXISTS admin_rota;
+DROP TABLE IF EXISTS feedback;
+DROP TABLE IF EXISTS partida;
+DROP TABLE IF EXISTS ponto;
+DROP TABLE IF EXISTS rota;
+DROP TABLE IF EXISTS rota_ponto;
 
 CREATE TABLE IF NOT EXISTS admin (
-    id SERIAL PRIMARY KEY UNIQUE ,
-    nome TEXT NOT NULL ,
-    email TEXT NOT NULL UNIQUE ,
-    senha TEXT NOT NULL
-)
+    id SERIAL PRIMARY KEY UNIQUE,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS admin_rota (
+    id SERIAL PRIMARY KEY UNIQUE,
+    cargo SMALLINT NOT NULL,
+    admin_id INT NOT NULL,
+    rota_id INT NOT NULL,
+    FOREIGN KEY (admin_id) REFERENCES admin,
+    FOREIGN KEY (rota_id) REFERENCES rota
+);
+
+CREATE TABLE IF NOT EXISTS feedback (
+    id SERIAL PRIMARY KEY UNIQUE,
+    avaliacao FLOAT NOT NULL,
+    nome VARCHAR(100),
+    mensagem VARCHAR,
+    rota_id INT NOT NULL,
+    FOREIGN KEY (rota_id) REFERENCES rota
+);
+
+CREATE TABLE IF NOT EXISTS partida (
+    id SERIAL PRIMARY KEY UNIQUE,
+    hora SMALLINT NOT NULL,
+    minuto SMALLINT NOT NULL,
+    dia_semana SMALLINT NOT NULL,
+    rota_id INT NOT NULL,
+    FOREIGN KEY (rota_id) REFERENCES rota
+);
+
+CREATE TABLE IF NOT EXISTS ponto (
+    id SERIAL PRIMARY KEY UNIQUE,
+    endereco VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS rota (
+    id SERIAL PRIMARY KEY UNIQUE,
+    codigo INT NOT NULL,
+    origem VARCHAR(30) NOT NULL,
+    destino VARCHAR(30) NOT NULL,
+    via VARCHAR(60)
+);
+
+
+CREATE TABLE IF NOT EXISTS rota_ponto (
+    id SERIAL PRIMARY KEY UNIQUE,
+    rota_id INT NOT NULL,
+    ponto_id INT NOT NULL,
+    FOREIGN KEY (rota_id) REFERENCES rota,
+    FOREIGN KEY (ponto_id) REFERENCES ponto
+);
