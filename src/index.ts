ed7d0c7fbec59ca0a4a330d84fa97ad76d030613
João Victor Olivo <jvso@yahoo.com>
2025-10-19 14:37:26 -0300

    Initial config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Running!")
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+// Rotas
+
+// Inicialização
+const PORT = process.env.PORT || 8800;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
