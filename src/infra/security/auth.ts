e2f0026acc90dbb0f1bbb50393b31f98d535e7a7
João Victor Olivo <jvso@yahoo.com>
2025-10-19 22:38:47 -0300

    JWT Authentication middleware

---
+++ src/infra/security/auth.ts
@@ -0,0 +1,33 @@
+import type { NextFunction, Request, Response } from 'express';
+import { HttpError } from 'infra/error/error-classes';
+import jwt, { type JwtPayload } from 'jsonwebtoken';
+
+export function auth(req: Request, res: Response, next: NextFunction) {
+	const token = req.headers.authorization?.split(" ")[1]; // "Bearer <token>"
+	if (!token) throw new HttpError(401, "No Token Provided");
+
+	if (process.env.JWT_SECRET == undefined)
+		throw new Error("JWT_SECRET não foi definido como variável de ambiente");
+
+	try {
+		// Decodifica e armazena dados do usuário no request
+		const payload = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload & typeof req.user | string;
+		if (typeof payload == 'string')
+			// noinspection ExceptionCaughtLocallyJS
+			throw new jwt.JsonWebTokenError("Invalid Token");
+
+		req.user = {
+			id: payload.id
+		}
+
+		next();
+	} catch (e) {
+		if (e instanceof jwt.JsonWebTokenError)
+			throw new HttpError(401, "Invalid Token");
+		if (e instanceof jwt.TokenExpiredError)
+			throw new HttpError(401, "Token Expired");
+		if (e instanceof jwt.NotBeforeError)
+			throw new HttpError(401, "Token is not valid yet");
+		throw e;
+	}
+}
