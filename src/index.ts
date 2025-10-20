985ef8f548ba85814c065c27cf4581a967717716
João Victor Olivo <jvso@yahoo.com>
2025-10-20 19:36:17 -0300

    Routers now defined at index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import express from 'express';
+/// <reference path="types/express.d.ts" />
+
 import dotenv from 'dotenv';
+import express from 'express'
+import { auth } from 'middleware/auth';
+import * as authController from 'controller/auth-controller';
+import * as admin from 'controller/admin';
 dotenv.config();
 
 import auth from 'router/auth';
@@ -10,6 +15,12 @@ app.use(express.json());
 
 // Rotas
 app.use('', auth);
+app.post('/register', authController.register);
+app.post('/login', authController.login);
+app.get('/admin', auth, admin.get);
+app.put('/admin', auth, admin.put);
+app.delete('/admin', auth, admin.remove);
+
 
 // Inicialização
 const PORT = process.env.PORT || 8800;
--- src/router/auth.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { register, login } from '../controller/auth';
-
-const router = express.Router();
-
-router.post('/register', register);
-router.post('/login', login);
-
-export default router;
