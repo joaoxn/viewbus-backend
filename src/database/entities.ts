29b3076837a1e460e175e85a94829385e9b07a98
João Victor Olivo <jvso@yahoo.com>
2025-10-20 21:14:00 -0300

    Defined type of all entities

--- src/database/entities.ts
+++ src/database/entities.ts
@@ -9,6 +9,42 @@ export type Admin = {
 	senha: string
 }
 
+export type Rota = {
+	id: number,
+	codigo: number,
+	origem: string,
+	destino: string,
+	via: string | undefined,
+	admin_id: number
+}
+
+export type Ponto = {
+	id: number,
+	endereco: string
+}
+
+export type Rota_Ponto = {
+	id: number,
+	rota_id: number,
+	ponto_id: number
+}
+
+export type Partida = {
+	id: number,
+	hora: number,
+	minuto: number,
+	dia_semana: number,
+	rota_id: number
+}
+
+export type Feedback = {
+	id: number,
+	avaliacao: number,
+	nome: string,
+	mensagem: string,
+	rota_id: number
+}
+
 export function getAdmin(req: Request): DTO<Admin> {
 	const nome = req.body.nome;
 	const email = req.body.email;
